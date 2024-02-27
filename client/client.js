73ad81c8efb0def91aad3ee480f56a8e33461a94
Colin C <me@thecolin.co>
2024-02-27 18:46:09 -0500

    Ready for demo

--- client/client.js
+++ client/client.js
@@ -15,24 +15,3 @@ function addMessage(color, name, msg) {
 
 
 // Your code here
-const socket = new WebSocket('ws://localhost:8080');
-socket.addEventListener('open', function() {
-    addNotification("Connected!");
-});
-
-socket.addEventListener('close', function() {
-    addNotification("Connection Closed!");
-});
-
-socket.addEventListener('message', function (msg) {
-    const data = JSON.parse(msg.data);
-    addMessage(data.color, data.name, data.text);
-})
-
-submitButton.addEventListener('click', function(e) {
-    console.log(nameInput.value);
-    socket.send(JSON.stringify({
-        name: nameInput.value || "Unknown User",
-        text: msgInput.value || "",
-    }));
-})
